3bd90a729d09a4102e4fc8c9cc618d790a89413b
netricks <mirmikns@yandex.ru>
2025-11-29 21:41:29 +0300

    'a'

--- macro.h
+++ macro.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define GUARD_STRINGIFY(...) #__VA_ARGS__
-#define GUARD_STRINGIFY2(...) STRINGIFY(__VA_ARGS__)
+#define GUARD_STRINGIFY2(...) GUARD_STRINGIFY_IMPL(__VA_ARGS__)
 
 #define GUARD_CONCAT(a, b) a##b
 #define GUARD_CONCAT2(a, b) GUARD_CONCAT(a, b)
